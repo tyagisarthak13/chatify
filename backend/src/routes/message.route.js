94c228a634c3a38ae6e3ecaf78197ffacf32c7a9
tyagisarthak13 <sarthaktyagi797@gmail.com>
2026-01-20 22:09:10 +0530

    message routes and controllers added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,20 @@
 import express from "express";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+  getChatPartners,
+} from "../contollers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import arcjetProtection from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Send message endpoint");
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
